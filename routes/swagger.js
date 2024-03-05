9fb7bf058c177c5f0cc701044aeb8ce37e905487
LolaCuadra <lola_cuadra1940@byui.edu>
2024-03-04 22:14:10 -0700

    push

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,7 +3,6 @@ const { auth, requiresAuth } = require('express-openid-connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swaggerDesign.json'); // don't want to override this with npm run swagger
 router.use('/api-docs', swaggerUi.serve);
-// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 router.get('/api-docs', requiresAuth(),swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
